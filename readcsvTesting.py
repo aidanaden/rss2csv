f1a620e796bbfe4e2d35480c2c650aad0821184b
Ryan Aidan <aidanaden@hotmail.com>
2017-09-06 16:50:10 +0800

    Add files via upload

--- readcsvTesting.py
+++ readcsvTesting.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import feedparser
 import csv
@@ -34,46 +35,66 @@ def EDT2SGConverter(time):
 	
 		return date_sg_timezone
 
-def rss2csv(url, categoryValue):
+def LastModifiedDateTimeIsOlder(feed_UTC):
+
+	#convert both datetimes to utc to do calculation, 
+	#if possible would be better to do calculation with sgt
+	exitCode = ""
+	#sample = "2017-04-27 13:31:00+08:00"
+	dateutil_converted = dateutil.parser.parse(str(feed_UTC))
+	#utcSample = sgt2utc(sample)
+	cwd = os.path.getmtime(news_feed_file)
+	converted = datetime.datetime.utcfromtimestamp(cwd)
+	converted_tz = datetime.datetime(year=converted.year,month=converted.month,day=converted.day,
+		hour=converted.hour,minute=converted.minute,second=converted.second,tzinfo=pytz.utc)
+	print(converted_tz)
+	try:
+		if converted_tz < dateutil_converted:
+			return True
+		else:
+			return False
+	except:
+		if converted < dateutil_converted:
+			return True
+		else:
+			return False
+
+
+def getFileNameFromURL(url):
+	#create special char table to be removed
+	special_char_table = str.maketrans(dict.fromkeys('?./='))
+	#remove http and https from url
+	filename = url.replace('http://', '').replace('https://', '')
+	#remove special characters
+	cleaned_filename = filename.translate(special_char_table)
+
+	return cleaned_filename
+
+def rss2csv(url, categoryValue, dict_writer):
 
 	#parse feed using feedparser
 	feed = feedparser.parse(url)
 
-	#create special char table to be removed
-	special_char_table = str.maketrans(dict.fromkeys('?./='))
-
-	#remove http and https from url
-	filename = url.replace('http://', '').replace('https://', '')
-
-	#remove special characters
-	cleaned_filename = filename.translate(special_char_table)
+	#filename = getFileNameFromURL(url)
 	
-	#toCSVKeys = feed['entries'][0].keys()
-	fieldname1 = 'article_published'
-	fieldname2 = 'article_title'
-	fieldname3 = 'article_description'
-	fieldname4 = 'aricle_url'
-	fieldname5 = 'category'
-
-	fieldNames = [fieldname1, fieldname2, fieldname3, fieldname4, fieldname5]
-
 	dateFields = ['published','pubDate','date','updated']
 	titleFields = ['title','id']
 	summaryFields = ['summary','description']
 
-	with open(cleaned_filename + '.csv', "wt", encoding = "utf-8") as output_file:
-		dict_writer = csv.DictWriter(output_file, fieldNames)
-		dict_writer.writeheader()
-		for entry in feed['entries']:
+	for entry in feed['entries']:
 
-			# commented try block for category
-			#try:	
-			#	categoryValue = entry['category']
-			#except:
-			#	categoryValue = "NIL"
-			converted_sg_time = "NA"
-			summaryEntry = "NA"
-			titleEntry = "NA"
+		converted_sg_time = "NA"
+		summaryEntry = "NA"
+		titleEntry = "NA"
+
+		for dateField in dateFields:
+			try:
+				shouldDownload = LastModifiedDateTimeIsOlder(entry[dateField])
+				converted_sg_time = utc2sgt(entry[dateField])
+			except:
+				continue
+		print(shouldDownload)		
+		if shouldDownload | len(df.index) <= 2:
 
 			for summaryField in summaryFields:
 				try:
@@ -87,24 +108,32 @@ def rss2csv(url, categoryValue):
 				except:
 					continue
 
-			for dateField in dateFields:
-				try:
-					converted_sg_time = utc2sgt(entry[dateField])
-				except:
-					continue
+		
 
 			dict_writer.writerow({fieldname1:converted_sg_time,fieldname2:titleEntry,fieldname3:summaryEntry,fieldname4:entry['link'], fieldname5:categoryValue})
 
-		pass
+	pass
 
 news_feed_file = 'News Feeds.csv'
+output_filename = "output_feeds.csv"
+
+fieldname1 = 'article_published'
+fieldname2 = 'article_title'
+fieldname3 = 'article_description'
+fieldname4 = 'aricle_url'
+fieldname5 = 'category'
+
+fieldNames = [fieldname1, fieldname2, fieldname3, fieldname4, fieldname5]
+
 with open(news_feed_file, 'rt', encoding = 'utf-8') as input_file:
 	df = pandas.read_csv(input_file)
-	for column in df:
-		for row in df[column]:
-			#print(row)
-			#print(column)
-			rss2csv(str(row), str(column))
+	with open(output_filename, "wt", encoding = "utf-8") as output_file:
+		dict_writer = csv.DictWriter(output_file, fieldNames)
+		dict_writer.writeheader()
+		print(len(df.index))
+		for column in df:
+			for row in df[column]:
+				rss2csv(str(row), str(column), dict_writer)
 			
 		
 		
