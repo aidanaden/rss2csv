c3102dfdb3fefe2a2626930e25ef885b3ccbd18e
Ryan Aidan <aidanaden@hotmail.com>
2017-09-06 14:28:44 +0800

    cleaned data

--- readcsvTesting.py
+++ readcsvTesting.py
@@ -8,6 +8,10 @@ import dateutil.parser
 from dateutil import tz
 
 def utc2sgt(time):
+
+	if time is None:
+		return "NA"
+
 	date = dateutil.parser.parse(time)
 	from_zone = tz.tzutc()
 	to_zone = tz.tzlocal()
@@ -54,6 +58,9 @@ def rss2csv(url, categoryValue):
 	fieldNames = [fieldname1, fieldname2, fieldname3, fieldname4, fieldname5]
 
 	dateFields = ['published','pubDate','date','updated']
+	titleFields = ['title','id']
+	summaryFields = ['summary','description']
+
 	with open(cleaned_filename + '.csv', "wt", encoding = "utf-8") as output_file:
 		dict_writer = csv.DictWriter(output_file, fieldNames)
 		dict_writer.writeheader()
@@ -64,6 +71,21 @@ def rss2csv(url, categoryValue):
 			#	categoryValue = entry['category']
 			#except:
 			#	categoryValue = "NIL"
+			converted_sg_time = "NA"
+			summaryEntry = "NA"
+			titleEntry = "NA"
+
+			for summaryField in summaryFields:
+				try:
+					summaryEntry = entry[summaryField]
+				except:
+					continue
+
+			for titleField in titleFields:
+				try:
+					titleEntry = entry[titleField]
+				except:
+					continue
 
 			for dateField in dateFields:
 				try:
@@ -71,7 +93,7 @@ def rss2csv(url, categoryValue):
 				except:
 					continue
 
-			dict_writer.writerow({fieldname1:converted_sg_time,fieldname2:entry['title'],fieldname3:entry['summary'],fieldname4:entry['link'], fieldname5:categoryValue})
+			dict_writer.writerow({fieldname1:converted_sg_time,fieldname2:titleEntry,fieldname3:summaryEntry,fieldname4:entry['link'], fieldname5:categoryValue})
 
 		pass
 
