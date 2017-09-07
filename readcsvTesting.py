9b85f6c2b9a02199efa65ca04548e749d10aacf1
Ryan Aidan <aidanaden@hotmail.com>
2017-09-07 17:27:12 +0800

    v3, converts date time properly
    requires adding url to dictionaries to check for relevant time zones

--- readcsvTesting.py
+++ readcsvTesting.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import feedparser
 import csv
@@ -7,48 +8,182 @@ import pytz
 import dateutil.parser
 from dateutil import tz
 
-def utc2sgt(time):
+def convert2sgt(time,url):
 
 	if time is None:
 		return "NA"
 
-	date = dateutil.parser.parse(time)
-	from_zone = tz.tzutc()
 	to_zone = tz.tzlocal()
+	date = dateutil.parser.parse(time)
 
-	utc = date.replace(tzinfo=from_zone)
-	local = utc.astimezone(to_zone)
+	if url in EDTURL:
+		from_zone = tz.gettz('US/Eastern')
+		edt = date.replace(tzinfo=from_zone)
+		local = edt.astimezone(to_zone)
+
+	elif url in PDTURL:
+		from_zone = tz.gettz('US/Pacific')
+		pdt = date.replace(tzinfo=from_zone)
+		local = pdt.astimezone(to_zone)
+
+	else:
+		from_zone = tz.tzutc()
+		utc = date.replace(tzinfo=from_zone)
+		local = utc.astimezone(to_zone)
 
 	return local
 
-def EDT2SGConverter(time):
+#def EDT2SGConverter(time):
 
-	if time.endswith('EDT'):
-		new_time = time[:-4]
+	#if time.endswith('EDT'):
+		#new_time = time[:-4]
 
-		original_fmt = "%a, %d %b %Y %H:%M:%S"
+		#original_fmt = "%a, %d %b %Y %H:%M:%S"
 
-		date = datetime.datetime.strptime(new_time, original_fmt)
-		dateLocalized = date + datetime.timedelta(days=1/2)
-		date_sg_timezone = str(dateLocalized) + " SGT"
+		#date = datetime.datetime.strptime(new_time, original_fmt)
+		#dateLocalized = date + datetime.timedelta(days=1/2)
+		#date_sg_timezone = str(dateLocalized) + " SGT"
 	
-		return date_sg_timezone
+		#return date_sg_timezone
 
-def rss2csv(url, categoryValue):
+def LastModifiedDateTimeIsOlder(feed_UTC):
+
+	#convert both datetimes to utc to do calculation, 
+	#if possible would be better to do calculation with sgt
+
+	#converted version of feed's data time: expected to be in utc else will fk up
+	feed_converted = dateutil.parser.parse(str(feed_UTC))
+	
+	cwd = os.path.getmtime(output_filename)
+	converted = datetime.datetime.utcfromtimestamp(cwd)
+	#convert timestamp of last modified to utc + add tzone awareness
+	converted_tz = datetime.datetime(year=converted.year,month=converted.month,day=converted.day,
+		hour=converted.hour,minute=converted.minute,second=converted.second,tzinfo=pytz.utc)
+	try:
+		#compare if feed date time is tz aware
+		if converted_tz < feed_converted:
+			return True
+		else:
+			return False
+	except:
+		#compare if feed date time is tz naive
+		if converted < feed_converted:
+			return True
+		else:
+			return False
+
+
+#def getFileNameFromURL(url):
+	##create special char table to be removed
+	#special_char_table = str.maketrans(dict.fromkeys('?./='))
+	##remove http and https from url
+	#filename = url.replace('http://', '').replace('https://', '')
+	##remove special characters
+	#cleaned_filename = filename.translate(special_char_table)
+
+	#return cleaned_filename
+
+def rss2csv(url, categoryValue, dict_writer, download):
 
 	#parse feed using feedparser
 	feed = feedparser.parse(url)
 
-	#create special char table to be removed
-	special_char_table = str.maketrans(dict.fromkeys('?./='))
-
-	#remove http and https from url
-	filename = url.replace('http://', '').replace('https://', '')
-
-	#remove special characters
-	cleaned_filename = filename.translate(special_char_table)
+	#filename = getFileNameFromURL(url)
 	
-	#toCSVKeys = feed['entries'][0].keys()
+	#create fields arrays to check
+	dateFields = ['published','pubDate','date','updated']
+	titleFields = ['title','id']
+	summaryFields = ['summary','description']
+	shouldDownload = False
+
+	for entry in feed['entries']:
+
+		converted_sg_time = "NA"
+		summaryEntry = "NA"
+		titleEntry = "NA"
+
+		for dateField in dateFields:
+			try:
+				shouldDownload = LastModifiedDateTimeIsOlder(entry[dateField])
+			except:
+				pass
+			else:
+				converted_sg_time = convert2sgt(entry[dateField],url)
+
+		if shouldDownload | download:
+
+			for summaryField in summaryFields:
+				try:
+					summaryEntry = entry[summaryField]
+				except:
+					pass
+
+			for titleField in titleFields:
+				try:
+					titleEntry = entry[titleField]
+				except:
+					pass
+
+			print("downloaded " + titleEntry)
+			dict_writer.writerow({fieldname1:converted_sg_time,fieldname2:titleEntry,
+				fieldname3:summaryEntry,fieldname4:entry['link'],fieldname5:categoryValue})
+
+	pass
+
+
+#with open(news_feed_file, 'rt', encoding = 'utf-8') as input_file:
+	#df = pandas.read_csv(input_file)
+	#with open(output_filename, "wt", encoding = "utf-8") as output_file:
+		#dict_writer = csv.DictWriter(output_file, fieldNames)
+		#dict_writer.writeheader()
+		#try:
+			#with open(output_filename, 'rt', encoding = 'utf-8') as input_output_file:
+				#output_df = pandas.read_csv(input_output_file)
+				#print(len(output_df.index))
+		#except:
+			#print('unable to read output file')
+			
+
+		#for column in df:
+			#for row in df[column]:
+				#rss2csv(str(row), str(column), dict_writer)
+
+
+def firstDownload():
+	shouldDownload = True
+	with open(news_feed_file, 'rt', encoding = 'utf-8') as input_file:
+		df = pandas.read_csv(input_file)
+		with open(output_filename, "wt", encoding = "utf-8") as output_file:
+			dict_writer = csv.DictWriter(output_file, fieldNames)
+			dict_writer.writeheader()
+			for column in df:
+				for row in df[column]:
+					rss2csv(str(row), str(column), dict_writer,True)
+
+def update():
+	with open(news_feed_file, 'rt', encoding = 'utf-8') as input_file:
+		df = pandas.read_csv(input_file)
+		with open(output_filename, "at", encoding = "utf-8") as output_file:
+			dict_writer = csv.DictWriter(output_file, fieldNames)
+			for column in df:
+				for row in df[column]:
+					rss2csv(str(row), str(column), dict_writer,False)
+
+#def sortCSVByDate():
+
+
+if __name__ == "__main__":
+
+	# time zones for urls : update these dictionaries 
+	# for future urls that use data times that aren't in utc
+	
+	EDTURL = ["https://phys.org/rss-feed/technology-news/security/","http://www.darkreading.com/rss_simple.asp",
+			  "https://phys.org/rss-feed/technology-news/security/","http://feeds.reuters.com/reuters/technologyNews"]
+	PDTURL = ["http://feeds.feedburner.com/feedburner/Talos?format=xml","http://feeds.feedburner.com/TheHackersNews"]
+
+	news_feed_file = 'News Feeds.csv'
+	output_filename = "output_feeds.csv"
+
 	fieldname1 = 'article_published'
 	fieldname2 = 'article_title'
 	fieldname3 = 'article_description'
@@ -57,56 +192,20 @@ def rss2csv(url, categoryValue):
 
 	fieldNames = [fieldname1, fieldname2, fieldname3, fieldname4, fieldname5]
 
-	dateFields = ['published','pubDate','date','updated']
-	titleFields = ['title','id']
-	summaryFields = ['summary','description']
+	usageMessage = "\nUsage: python readcsvTesting.py <command>\nExample: python readcsvTesting.py download\n\nOptions:\n1) download : download all rss feeds\n2) update : update rss feeds"
+	try:
+		arg1 = sys.argv[1]
+		if arg1 == 'download':
+			firstDownload()
+		elif arg1 == 'update':
+			update()
+		else:
+			print(usageMessage)
 
-	with open(cleaned_filename + '.csv', "wt", encoding = "utf-8") as output_file:
-		dict_writer = csv.DictWriter(output_file, fieldNames)
-		dict_writer.writeheader()
-		for entry in feed['entries']:
+	except IndexError:
+		print(usageMessage)
+		sys.exit()
 
-			# commented try block for category
-			#try:	
-			#	categoryValue = entry['category']
-			#except:
-			#	categoryValue = "NIL"
-			converted_sg_time = "NA"
-			summaryEntry = "NA"
-			titleEntry = "NA"
-
-			for summaryField in summaryFields:
-				try:
-					summaryEntry = entry[summaryField]
-				except:
-					continue
-
-			for titleField in titleFields:
-				try:
-					titleEntry = entry[titleField]
-				except:
-					continue
-
-			for dateField in dateFields:
-				try:
-					converted_sg_time = utc2sgt(entry[dateField])
-				except:
-					continue
-
-			dict_writer.writerow({fieldname1:converted_sg_time,fieldname2:titleEntry,fieldname3:summaryEntry,fieldname4:entry['link'], fieldname5:categoryValue})
-
-		pass
-
-news_feed_file = 'News Feeds.csv'
-with open(news_feed_file, 'rt', encoding = 'utf-8') as input_file:
-	df = pandas.read_csv(input_file)
-	for column in df:
-		for row in df[column]:
-			#print(row)
-			#print(column)
-			rss2csv(str(row), str(column))
-			
-		
 		
 
 
